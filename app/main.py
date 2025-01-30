c427ecfbd999f5f04c76ab2acf7074d8ffbaf1ad
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-01-30 10:36:10 +0530

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+from fastapi import FastAPI
+
+from app.routes.friend_routes import router as friend_router
+from app.routes.location_routes import router as location_router
+from app.routes.safe_place_routes import router as safe_place_router
+
+app = FastAPI()
+
+app.include_router(friend_router)
+app.include_router(location_router)
+app.include_router(safe_place_router)
+
+
+#uvicorn app.main:app --reload --port 8000 --host 0.0.0.0
